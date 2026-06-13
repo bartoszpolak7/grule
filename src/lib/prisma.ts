0738eb5631eb473fdb2ad3ae1e3b081723e09690
szczuras7 <szczuras7@proton.me>
2026-06-13 12:33:33 +0200

    fix prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,10 +1,15 @@
-import { PrismaClient } from "@prisma/client/extension";
+import { PrismaClient } from "../generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 
 // definiuje jednego klienta dla developmentu, aby nie tworzyć za każdym razem nowego
 // w przypadku Hot Module Reload
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient();
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    adapter: new PrismaPg({ connectionString: process.env.DATABASE_URL }),
+  });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
